f40881e853e5232a4e9231d062ffed4d9e889b49
toarupgm <toarupgm@gmail.com>
2022-09-13 07:23:41 +0900

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
     var request = require('request');
 
     var options = {
-            url: 'https://raw.githubusercontent.com/Mauler125/r5sdk/5f56e5abd3f3df1c45bf3a07f64e91daaf2bc0dd/r5dev/tier0/basetypes.h',
+        url: 'https://raw.githubusercontent.com/Mauler125/r5sdk/5f56e5abd3f3df1c45bf3a07f64e91daaf2bc0dd/r5dev/tier0/basetypes.h',
         method: 'GET'
     }
     request(options, function (error, response, body) {
@@ -35,7 +35,41 @@ app.get('/', (req, res) => {
             request.post(options, function(error, response, body){
                 if(body["success"])
                 {
-                    res.render('index',{servers:body["servers"]})
+                    const servers = body["servers"];
+                    const players = [];
+                    for (let i = 0; i < servers.length; i++) {
+                        players.push(parseInt(servers[i]["playerCount"]))
+                    }
+                    var max = Math.max.apply(null,players);
+
+                    for (let i = 0; i < servers.length; i++) {
+                        servers[i]["top"] = servers[i]["playerCount"]==max;
+                    }
+
+
+                    const maps = {
+                        "mp_rr_canyonlands_64k_x_64k":"King's Canyon Season 0",
+                        "mp_rr_desertlands_64k_x_64k":"World's Edge Season 3",
+                        "mp_rr_canyonlands_mu1":"King's Canyon Season 2",
+                        "mp_rr_canyonlands_mu1_night":"King's Canyon Season 2 After Dark",
+                        "mp_rr_desertlands_64k_x_64k_nx":"World's Edge Season 3 After Dark",
+                        "mp_lobby":"Lobby Season 3",
+                        "mp_rr_canyonlands_staging":"King's Canyon Firing Range",
+                        "mp_rr_arena_composite":"Arena Map",
+                        "mp_rr_aqueduct":"Drop Off",
+                        "mp_rr_aqueduct_night":"Drop Off Night"
+                    }
+                    for (let i = 0; i < servers.length; i++) {
+                        if(Object.keys(maps).includes(servers[i]["map"]))
+                        {
+                            servers[i]["displaymap"] = maps[servers[i]["map"]];
+                        }else{
+                            servers[i]["displaymap"] = "CustomMap: " + servers[i]["map"];
+                        }
+                    }
+                    
+
+                    res.render('index',{"servers":servers,"max":max})
                 }
             });
     })
